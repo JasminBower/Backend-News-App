ee8a2b1d9dfa7268b48d329136bd108e3abb2b06
JasminBower <jasmin.ibower@gmail.com>
2020-05-21 17:01:18 +0100

    removed unused code

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,55 +1,44 @@
-exports.formatDates = list => {
-   const arrayCopy = [...list];
-   const updatedComment = [];
+exports.formatDates = (list) => {
+	const updatedComment = [];
 
-   arrayCopy.forEach(comment => {
-       const commentCopy = {...comment};
-       
-       commentCopy.created_at = new Date(commentCopy.created_at);
-      
-       updatedComment.push(commentCopy);
-   });
-   return updatedComment;
+	list.forEach((comment) => {
+		const commentCopy = { ...comment };
 
+		commentCopy.created_at = new Date(commentCopy.created_at);
+
+		updatedComment.push(commentCopy);
+	});
+	return updatedComment;
 };
 
-exports.makeRefObj = list => {
-    const arrayCopy = [...list];
+exports.makeRefObj = (list) => {
+	const updatedItem = {};
 
-    const updatedItem = {};
-
-    arrayCopy.forEach(item => {
-      updatedItem[item.title] = item.article_id;
-
-    });
-    return updatedItem
-    
+	list.forEach((item) => {
+		updatedItem[item.title] = item.article_id;
+	});
+	return updatedItem;
 };
 
 exports.formatComments = (comments, articleRef) => {
-    const commentsCopy = comments.map(comment => {
-        return {...comment}
-    });
+	const commentsCopy = comments.map((comment) => {
+		return { ...comment };
+	});
 
-    const modifiedComments = [];
+	const modifiedComments = [];
 
-    commentsCopy.forEach(comment => {
-        
-         for(let key in articleRef){
-            if(key === comment.belongs_to){
-                comment.author = comment.created_by;
-                comment.created_at = new Date(comment.created_at);
-                comment.article_id = articleRef[key];
-                delete comment.belongs_to;
-                delete comment.created_by;
-               
-               
-            }
-          
-        }
-        modifiedComments.push(comment)
-   })
-   
-    return modifiedComments;
+	commentsCopy.forEach((comment) => {
+		for (let key in articleRef) {
+			if (key === comment.belongs_to) {
+				comment.author = comment.created_by;
+				comment.created_at = new Date(comment.created_at);
+				comment.article_id = articleRef[key];
+				delete comment.belongs_to;
+				delete comment.created_by;
+			}
+		}
+		modifiedComments.push(comment);
+	});
 
+	return modifiedComments;
 };
