d80800fea74badf6858004fc2866b1ff060b3e0d
JasminBower <jasmin.ibower@gmail.com>
2020-05-21 17:00:02 +0100

    removed usued code and moved error handling to different file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const { errorHandler } = require("./errorhandlng");
 const { apiRouter } = require("./routes/api-router");
 
 app.use(express.json());
@@ -12,18 +12,7 @@ app.use("/api", apiRouter);
 app.use("/*", (req, res, next) => {
 	res.status(404).send({ msg: "Path not found" });
 });
-app.all("*", (req, res, next) => {
-	res.status(405).send({ msg: "Method not allowed" });
-});
-app.use((err, req, res, next) => {
-	if (err.code === "23503" || err.code === "22003")
-		res.status(404).send({ msg: "article not found" });
-	if (err.code === "22P02" || err.code === "42703")
-		res.status(400).send({ msg: "Bad Request" });
 
-	if (err.status) res.status(err.status).send({ msg: err.msg });
-
-	res.status(500).send({ msg: "SERVER ERROR" });
-});
+app.use(errorHandler);
 
 module.exports = app;
